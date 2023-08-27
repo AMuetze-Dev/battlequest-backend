913316d6de259dcbc3520f65e309501453066a64
Wayssm <aaron.muetze@outlook.com>
2023-08-27 19:47:43 +0200

    init

---
+++ src/main/java/de/amit/battlequest/BattlequestApplication.java
@@ -0,0 +1,13 @@
+package de.amit.battlequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/amit/battlequest/BattlequestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.amit.battlequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
