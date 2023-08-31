003222066e1416f196103926b0ec3df4f6151f3f
Wayssm <aaron.muetze@outlook.com>
2023-08-31 16:56:06 +0200

    Methoden sortiert, unnötige Imports entfernt, Beispielsklassen entfernt

--- src/main/java/de/amit/battlequest/controller/login/PlayerRepository.java
+++ src/main/java/de/amit/battlequest/controller/login/PlayerRepository.java
@@ -1,12 +1,9 @@
 package de.amit.battlequest.controller.login;
 
-import de.amit.battlequest.model.Player;
-import de.amit.battlequest.model.Test;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import de.amit.battlequest.model.Player;
 
-public interface PlayerRepository extends JpaRepository<Player, Long>{
-    public Player findByUsername(String username);
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+	public Player findByUsername(String username);
 }
--- src/main/java/de/amit/battlequest/model/Response.java
+++ src/main/java/de/amit/battlequest/model/Response.java
@@ -1,23 +1,27 @@
 package de.amit.battlequest.model;
 
 public class Response {
-    private boolean isSuccess;
-    private String message;
+	private boolean isSuccess;
+	private String message;
 
-    public Response(String message, boolean isSuccess) {
-        this.message = message;
-        this.isSuccess = isSuccess;
-    }
-    public boolean isSuccessful() {
-        return isSuccess;
-    }
-    public void setIsSuccess(boolean success) {
-        this.isSuccess = success;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	public Response(String message, boolean isSuccess) {
+		this.message = message;
+		this.isSuccess = isSuccess;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public boolean isSuccessful() {
+		return isSuccess;
+	}
+
+	public void setIsSuccess(boolean success) {
+		isSuccess = success;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
